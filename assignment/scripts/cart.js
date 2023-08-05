7900a1107e73b8ad792de0883d02fc5bff6541e8
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-04 20:49:37 -0500

    getting started, progress is good so far

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,25 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// this is our container
 let basket = []
 
+// this function will allow us to push an item into the array by using addItem(""
 
+function addItem(item) {
+    basket.push(item)
+    return true;
+}
 
+//attempting to add item Kale per the instructions and console log to confirm results
+addItem("kale");
+console.log(basket)
 
+//creating function to list items in the basket, needs to be a loop
 
+function listItems() {
 
-
+}
 
 
 
